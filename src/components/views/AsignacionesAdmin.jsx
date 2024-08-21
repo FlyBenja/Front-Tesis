86854a61640188ba6abdcbb4659fc914e5037cdf
FlyBenja <aldanabenjamin185@gmail.com>
2024-08-20 19:20:29 -0600

    Reestructuracion de carpetas y codigos, más mejoramiento en modo celular

--- src/components/views/AsignacionesAdmin.jsx
+++ src/components/views/AsignacionesAdmin.jsx
@@ -1,15 +1,8 @@
 import { useState } from 'react';
-import Navbar from '../Navbar/Navbar';
-import Sidebar from '../Sidebar/SidebarAdmin';
 import '../layout/Admin/AsignacionesAdmin.css';
 
 const AsignacionesAdmin = () => {
-  const [showSidebar, setShowSidebar] = useState(true);
   const [selectedFile, setSelectedFile] = useState(null);
-
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -21,45 +14,33 @@ const AsignacionesAdmin = () => {
   };
 
   return (
-    <div className={`wrapper ${showSidebar ? '' : 'sidebar-collapsed'} mt-0 AsignacionesAdmin`}>
-      <Sidebar showSidebar={showSidebar} />
-      <div className={`content-wrapper ${showSidebar ? 'expanded' : 'collapsed'}`}>
-        <Navbar toggleSidebar={toggleSidebar} showSidebar={showSidebar} />
-
-        {/* Ajuste de la clase para que el contenido admin se centre y se mueva correctamente */}
-        <div className="admin-content d-flex flex-column align-items-center">
-          <h2>Asignaciones</h2>
-
-          {/* Filtros para seleccionar curso y tipo */}
-          <div className="filters-container">
-            <select className="form-select form-select-curso">
-              <option>Cursos</option>
-              <option>Curso 1</option>
-              <option>Curso 2</option>
-            </select>
-            <select className="form-select form-select-estudiante">
-              <option>Área</option>
-              <option>Estudiantes</option>
-              <option>Catedrático</option>
-            </select>
-          </div>
-
-          {/* Área de subida de archivos */}
-          <div className="upload-section">
-            <input
-              type="file"
-              id="file-upload-input"
-              accept=".xlsx, .xls"
-              style={{ display: 'none' }}
-              onChange={handleFileChange}
-            />
-            <div className="upload-area" onClick={() => document.getElementById('file-upload-input').click()}>
-              Click to upload image
-            </div>
-            {selectedFile && <p className="file-info">Archivo seleccionado: {selectedFile.name}</p>}
-            <button className="confirm-button">Confirmar Subida</button>
-          </div>
+    <div className="asignaciones-admin d-flex flex-column align-items-center">
+      <h2>Asignaciones</h2>
+      <div className="filters-container">
+        <select className="form-select form-select-curso">
+          <option>Cursos</option>
+          <option>Curso 1</option>
+          <option>Curso 2</option>
+        </select>
+        <select className="form-select form-select-estudiante">
+          <option>Área</option>
+          <option>Estudiantes</option>
+          <option>Catedrático</option>
+        </select>
+      </div>
+      <div className="upload-section">
+        <input
+          type="file"
+          id="file-upload-input"
+          accept=".xlsx, .xls"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+        <div className="upload-area" onClick={() => document.getElementById('file-upload-input').click()}>
+          Click to upload image
         </div>
+        {selectedFile && <p className="file-info">Archivo seleccionado: {selectedFile.name}</p>}
+        <button className="confirm-button">Confirmar Subida</button>
       </div>
     </div>
   );
