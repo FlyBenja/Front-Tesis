90cf25e36f64736075622315e29ba68a13c66cd4
FlyBenja <aldanabenjamin185@gmail.com>
2024-08-23 18:44:18 -0600

    Le cambias el tipo a la imagen

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -2,7 +2,7 @@ import "admin-lte/dist/css/adminlte.min.css";
 import "../layout/Admin/Login.css";
 import loginLogo from "../../assets/imgs/login.svg"; // Imagen del muñeco
 import umgLogo from "../../assets/imgs/logo3.png"; // Logo de la UMG
-import ofiLogo from "/src/assets/imgs/sistemas1_11zon.webp"; // Ruta absoluta desde la carpeta src
+import ofiLogo from "/src/assets/imgs/sistemas1_11zon.jpg";
 import { useNavigate } from "react-router-dom";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 
